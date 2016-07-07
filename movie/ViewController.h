58abe9c30788be66f843162302f9b43b327ae6ab
xiwang1991 <644689175@qq.com>
2016-07-07 15:14:21 +0800

    first commit

---
+++ movie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  movie
+//
+//  Created by Pro on 15/10/15.
+//  Copyright (c) 2015年 enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ movie/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  movie
+//
+//  Created by Pro on 15/10/15.
+//  Copyright (c) 2015年 enterprise. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface ViewController ()
+{
+    MPMoviePlayerController *moviePlayer;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    UIWebView *web = [[UIWebView alloc] initWithFrame:self.view.bounds];
+//    NSString *str = @"http://www.youku.com";
+//    NSURL *url = [NSURL URLWithString:str];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [web loadRequest:request];
+//    [self.view addSubview:web];
+    
+    
+    NSURL *url = [NSURL URLWithString:@"http://175.6.7.244:8080/appdemo/xj.mp4"];
+    moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:url];
+
+    moviePlayer.view.frame = CGRectMake(0, 20, CGRectGetMaxX(self.view.bounds), CGRectGetHeight(self.view.bounds) * 0.3);
+    
+    moviePlayer.scalingMode = MPMovieScalingModeAspectFit;
+    
+    [moviePlayer setFullscreen:YES];
+    
+    [moviePlayer play];
+    
+    [self.view addSubview:moviePlayer.view];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
